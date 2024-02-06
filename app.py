7396c291582b8b9ac81695fad8be8bb831c5f1fc
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-02-06 21:05:14 +0300

    test modules folder

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import tkinter as tkinter
 
+import sys
+sys.path.insert(1, './modules')
+import test_module
+
+
+
 from tkinter import *
 root = Tk()
 
 def test_click():
     root.title('WAS TESTED')
+    test_module.module_func()
 
 main_window = Label(root, width = '100', height='40')
 test_button = Button(text = 'test button', command=test_click)
---
+++ modules/test_module.py
@@ -0,0 +1,2 @@
+def module_func():
+    print('HELLO MODULE')
