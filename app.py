cd486d4db71fcb2dc6a5e00a51d57b9aec446ce1
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-02-06 21:14:50 +0300

    final test modules

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import tkinter as tkinter
 
 import sys
 sys.path.insert(1, './modules')
-import test_module
+import test_module, test2
 
 
 
@@ -12,6 +12,7 @@ root = Tk()
 def test_click():
     root.title('WAS TESTED')
     test_module.module_func()
+    test2.module_func()
 
 main_window = Label(root, width = '100', height='40')
 test_button = Button(text = 'test button', command=test_click)
