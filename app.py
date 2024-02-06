7fb6493d1729440bd11c070ea6b08fe2d6003653
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-02-06 20:50:54 +0300

    Test tkinter lib

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print('hello Test Commit')
+import tkinter as tkinter
+
+from tkinter import *
+root = Tk()
+
+def test_click():
+    root.title('WAS TESTED')
+
+main_window = Label(root, width = '100', height='40')
+test_button = Button(text = 'test button', command=test_click)
+
+root.title('NOT TESTED')
+
+
+test_button.pack()
+main_window.pack()
+
+
+
+
+
+root.mainloop()
