63ddaf4ebe62eeed04be56d3d3a33249d6ab6009
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-02-09 20:08:37 +0300

    add label

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import sys
 sys.path.insert(1, './modules')
 import w_main
 
-w_main()
+w_main
--- modules/w_main.py
+++ modules/w_main.py
@@ -3,12 +3,22 @@ import tkinter as tk
 
 w_main = tk.Tk()
 
-
+#main window area
 w_main.geometry('800x500+400+150')
 w_main.title('КТСС')
 w_main.resizable(False, False)
 icon = tk.PhotoImage(file='./img/icon.png')
 w_main.iconphoto(False, icon)
+
+#label area
+l_title = tk.Label(w_main, text='Калькулятор Тарифов Сотовой Связи', 
+                   font=('Arial', 16, 'bold'), 
+                   height='3',
+                   bg='grey', fg='green')
+l_title.pack()
+
+#buttons area
+
 
 #для дальнейшей работы
 #w_main.config(bg='green')
