759b4755b6f3f6cf2057c5c3084df266222d6f86
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-02-15 20:00:56 +0300

    add edit button and logic

--- modules/main.py
+++ modules/main.py
@@ -4,10 +4,26 @@ import b_click, constants
 
 is_download = False
 
+def on_click(event):
+    
+    item_id = t_main.identify_row(event.y)
+    t_main.selection_set(item_id)
+    t_main.focus(item_id)
+
+    t_main.item(item_id, values=("New value 1", "New value 2"))
+    t_main.bind('<Return>', save_edit)
+
+def save_edit(event):
+    item_id = t_main.focus() 
+    values = [t_main.set(item_id, column) for column in t_main['columns']]
+    print('Saved values:', values)
+
 w_main = tk.Tk()
 t_main = ttk.Treeview(columns=constants.t_col, show='headings')
 main_menu = tk.Menu()
 file_menu= tk.Menu(tearoff=0)
+
+
 
 def download_data():
     global is_download
@@ -28,6 +44,104 @@ def delete_line():
     selected_item = t_main.selection()[0] ## get selected item
     t_main.delete(selected_item)
 
+def open_edit_window():
+
+    if t_main.focus() == '':
+        return
+    
+    item_id = t_main.focus() 
+
+    values = [t_main.set(item_id, column) for column in t_main['columns']]
+
+    def edit_row():
+        name = entry_name.get() or values[0]
+        subscr = entry_subscr.get() or values[1]
+        mins_in = entry_mins_in.get() or values[2]
+        mins_out = entry_mins_out.get() or values[3]
+        price_roum = entry_price_roum.get() or values[4]
+        price_in = entry_price_in.get() or values[5]
+        price_out = entry_price_out.get() or values[6]
+        free_sms = entry_free_sms.get() or values[7]
+        free_mms = entry_free_mms.get() or values[8]
+        price_sms = entry_price_sms.get() or values[9]
+        price_mms = entry_price_mms.get() or values[10]
+        free_mb = entry_free_mb.get() or values[11]
+        price_mb = entry_price_mb.get() or values[12]        
+        
+        t_main.item(item_id, values=(name, subscr, mins_in, mins_out, price_roum, price_in, price_out,
+                                     free_sms, free_mms, price_sms, price_mms, free_mb, price_mb))
+        new_window.destroy()   
+
+    new_window = tk.Toplevel(w_main)
+    new_window.title("Добавить тариф")
+    new_window.geometry('750x120+400+300')
+    new_window.resizable(False, False)
+    new_window.focus_set()
+    new_window.attributes("-topmost", True)
+    label = tk.Label(new_window, text="Введите характеристики тарифного плана")
+    label.grid(column='0', row='0', columnspan='13', sticky='wn', padx='4')
+
+    entry_name = tk.Entry(new_window, width='19')
+    entry_name.grid(column='1', row='2')
+    label1 = tk.Label(new_window, text='Тариф')
+    label1.grid(column='1', row='1')
+    entry_subscr = tk.Entry(new_window, width='8')
+    entry_subscr.grid(column='2', row='2')
+    label2 = tk.Label(new_window, text='Аб.Пл')
+    label2.grid(column='2', row='1')
+    entry_mins_in = tk.Entry(new_window, width='8')
+    entry_mins_in.grid(column='3', row='2')
+    label3 = tk.Label(new_window, text='Мин.В')
+    label3.grid(column='3', row='1')
+    entry_mins_out = tk.Entry(new_window, width='8')
+    entry_mins_out.grid(column='4', row='2')
+    label4 = tk.Label(new_window, text='Мин.Др')
+    label4.grid(column='4', row='1')
+    entry_price_roum = tk.Entry(new_window, width='8')
+    entry_price_roum.grid(column='5', row='2')
+    label5 = tk.Label(new_window, text='Ст.Роу')
+    label5.grid(column='5', row='1')
+    entry_price_in = tk.Entry(new_window, width='8')
+    entry_price_in.grid(column='6', row='2')
+    label6 = tk.Label(new_window, text='Ст.Вн.с')
+    label6.grid(column='6', row='1')
+    entry_price_out = tk.Entry(new_window, width='8')
+    entry_price_out.grid(column='7', row='2')
+    label7 = tk.Label(new_window, text='Ст.Др.с')
+    label7.grid(column='7', row='1')
+    entry_free_sms = tk.Entry(new_window, width='8')
+    entry_free_sms.grid(column='8', row='2')
+    label8 = tk.Label(new_window, text='К.СМС')
+    label8.grid(column='8', row='1')
+    entry_free_mms = tk.Entry(new_window, width='8')
+    entry_free_mms.grid(column='9', row='2')
+    label9 = tk.Label(new_window, text='К.ММС')
+    label9.grid(column='9', row='1')
+    entry_price_sms = tk.Entry(new_window, width='8')
+    entry_price_sms.grid(column='10', row='2')
+    label10 = tk.Label(new_window, text='Ст.СМС')
+    label10.grid(column='10', row='1')
+    entry_price_mms = tk.Entry(new_window, width='8')
+    entry_price_mms.grid(column='11', row='2')
+    label11 = tk.Label(new_window, text='Ст.ММС')
+    label11.grid(column='11', row='1')
+    entry_free_mb = tk.Entry(new_window, width='9')
+    entry_free_mb.grid(column='12', row='2')
+    label12 = tk.Label(new_window, text='Кол.Мб')
+    label12.grid(column='12', row='1')
+    entry_price_mb = tk.Entry(new_window, width='8')
+    entry_price_mb.grid(column='13', row='2')
+    label13 = tk.Label(new_window, text='Ст.Мб')
+    label13.grid(column='13', row='1') 
+    
+    button_add = tk.Button(new_window, text='Добавить', command=edit_row, width='10')
+    button_exit = tk.Button(new_window, text='Выход', command=new_window.destroy, width='10')
+    button_add.grid(column='10', row='3', pady='15', padx='10', columnspan='2', sticky='e')
+    button_exit.grid(column='12', row='3', pady='15', padx='10', columnspan='2', sticky='w')
+
+ 
+
+
 def open_fill_window():
     def add_row():
         name = entry_name.get()
@@ -47,6 +161,7 @@ def open_fill_window():
                                                                                price_in, price_out, free_sms, free_mms, price_sms,
                                                                                price_mms, free_mb, price_mb))
         new_window.destroy()
+        
 
     new_window = tk.Toplevel(w_main)
     new_window.title("Добавить тариф")
@@ -139,6 +254,7 @@ main_menu.add_cascade(label="Инструменты", menu=file_menu)
 file_menu.add_command(label='Загрузить список тарифов', command=download_data)
 file_menu.add_command(label='Вывести список тарифов', command=update_table)
 file_menu.add_command(label='Добавить тариф в список', command=add_data)
+file_menu.add_command(label='Редактировать тариф', command=open_edit_window)
 file_menu.add_command(label='Удалить тариф', command=delete_line)
 file_menu.add_separator()
 file_menu.add_command(label="Выйти", command=quit)
@@ -175,6 +291,8 @@ t_main.column('price_sms', width='50', minwidth='50')
 t_main.column('price_mms', width='50', minwidth='50')
 t_main.column('free_mb', width='50', minwidth='50')
 t_main.column('price_mb', width='50', minwidth='50')
+
+
 
 t_main.grid(column='1', row='1', rowspan='3', sticky='esn', padx='4')
 
