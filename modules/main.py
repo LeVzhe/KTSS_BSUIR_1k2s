2b4ca9589b191286b260eaf1a2e13643abc7550c
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-02-17 12:51:23 +0300

    fix: add validate in edit-window

--- modules/main.py
+++ modules/main.py
@@ -82,57 +82,31 @@ def open_edit_window():
 
     def edit_row():
         name = entry_name.get() or values[0]
-        if len(name) > 10:
-            name = values[0]
-            open_edit_window()
+
         subscr = entry_subscr.get() or values[1]
-        if (len(subscr) > 5) or (not is_number(subscr)):
-            subscr = values[1]
-            open_edit_window()
+
         mins_in = entry_mins_in.get() or values[2]
-        if (len(mins_in) > 5) or (not is_int(mins_in)):
-            mins_in = values[2]
-            open_edit_window()
+
         mins_out = entry_mins_out.get() or values[3]
-        if (len(mins_out) > 5) or (not is_int(mins_out)):
-            mins_out = values[3]
-            open_edit_window()
+
         price_roum = entry_price_roum.get() or values[4]
-        if (len(price_roum) > 5) or (not is_number(price_roum)):
-            price_roum = values[4]
-            open_edit_window()
+
         price_in = entry_price_in.get() or values[5]
-        if (len(price_in) > 5) or (not is_number(price_in)):
-            price_in = values[5]
-            open_edit_window()
+
         price_out = entry_price_out.get() or values[6]
-        if (len(price_out) > 5) or (not is_number(price_out)):
-            price_out = values[6]
-            open_edit_window()
+
         free_sms = entry_free_sms.get() or values[7]
-        if (len(free_sms) > 5) or (not is_int(free_sms)):
-            free_sms = values[7]
-            open_edit_window()
+
         free_mms = entry_free_mms.get() or values[8]
-        if (len(free_mms) > 5) or (not is_int(free_mms)):
-            free_mms = values[8]
-            open_edit_window()
+
         price_sms = entry_price_sms.get() or values[9]
-        if (len(price_sms) > 5) or (not is_number(price_sms)):
-            price_sms = values[9]
-            open_edit_window()
+
         price_mms = entry_price_mms.get() or values[10]
-        if (len(price_mms) > 5) or (not is_number(price_mms)):
-            price_mms = values[10]
-            open_edit_window()
+
         free_mb = entry_free_mb.get() or values[11]
-        if (len(free_mb) > 5) or (not is_int(free_mb)):
-            free_mb = values[11]
-            open_edit_window()
+
         price_mb = entry_price_mb.get() or values[12]
-        if (len(price_mb) > 5) or (not is_number(price_mb)):
-            price_mb = values[12]
-            open_edit_window()
+
         
         t_main.item(item_id, values=(name, subscr, mins_in, mins_out, price_roum, price_in , price_out,
                                      free_sms, free_mms, price_sms, price_mms, free_mb, price_mb))
@@ -148,79 +122,91 @@ def open_edit_window():
     label.grid(column='0', row='0', columnspan='2', sticky='wn', padx='4')
 
 
-    entry_name = tk.Entry(new_window, width='20', justify='right')
+    entry_name = tk.Entry(new_window, width='20', justify='right', validate='key', validatecommand=check_tarif_name)
     entry_name.bind('<FocusIn>', on_entry_click('', entry_name, values[0]))
     entry_name.bind('<FocusOut>', on_focusout('', entry_name, values[0]))
     entry_name.grid(column='1', row='1')
     label1 = tk.Label(new_window, text='Тариф')
     label1.grid(column='0', row='1', sticky='e')
-    entry_subscr = tk.Entry(new_window, width='20', justify='right')
+
+    entry_subscr = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_float)
     entry_subscr.grid(column='1', row='2')
     entry_subscr.bind('<FocusIn>', on_entry_click('', entry_subscr, values[1]))
     entry_subscr.bind('<FocusOut>', on_focusout('', entry_subscr, values[1]))
     label2 = tk.Label(new_window, text='Абонентская плата, руб.')
     label2.grid(column='0', row='2', sticky='e')
-    entry_mins_in = tk.Entry(new_window, width='20', justify='right')
+
+    entry_mins_in = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_int)
     entry_mins_in.grid(column='1', row='3')
     entry_mins_in.bind('<FocusIn>', on_entry_click('', entry_mins_in, values[2]))
     entry_mins_in.bind('<FocusOut>', on_focusout('', entry_mins_in, values[2]))
     label3 = tk.Label(new_window, text='Минут внутри сети')
     label3.grid(column='0', row='3', sticky='e')
-    entry_mins_out = tk.Entry(new_window, width='20', justify='right')
+
+    entry_mins_out = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_int)
     entry_mins_out.grid(column='1', row='4')
     entry_mins_out.bind('<FocusIn>', on_entry_click('', entry_mins_out, values[3]))
     entry_mins_out.bind('<FocusOut>', on_focusout('', entry_mins_out, values[3]))
     label4 = tk.Label(new_window, text='Минут в другие сети')
     label4.grid(column='0', row='4', sticky='e')
-    entry_price_roum = tk.Entry(new_window, width='20', justify='right')
+
+    entry_price_roum = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_float)
     entry_price_roum.grid(column='1', row='5')
     entry_price_roum.bind('<FocusIn>', on_entry_click('', entry_price_roum, values[4]))
     entry_price_roum.bind('<FocusOut>', on_focusout('', entry_price_roum, values[4]))
     label5 = tk.Label(new_window, text='Стоимость роуминга, руб.')
     label5.grid(column='0', row='5', sticky='e')
-    entry_price_in = tk.Entry(new_window, width='20', justify='right')
+
+    entry_price_in = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_float)
     entry_price_in.grid(column='1', row='6')
     entry_price_in.bind('<FocusIn>', on_entry_click('', entry_price_in, values[5]))
     entry_price_in.bind('<FocusOut>', on_focusout('', entry_price_in, values[5]))
     label6 = tk.Label(new_window, text='Стоимость внутри сети, руб.')
     label6.grid(column='0', row='6', sticky='e')
-    entry_price_out = tk.Entry(new_window, width='20', justify='right')
+
+    entry_price_out = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_float)
     entry_price_out.grid(column='1', row='7')
     entry_price_out.bind('<FocusIn>', on_entry_click('', entry_price_out, values[6]))
     entry_price_out.bind('<FocusOut>', on_focusout('', entry_price_out, values[6]))
     label7 = tk.Label(new_window, text='Стоимость на другие сети, руб.')
     label7.grid(column='0', row='7', sticky='e')
-    entry_free_sms = tk.Entry(new_window, width='20', justify='right')
+
+    entry_free_sms = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_int)
     entry_free_sms.grid(column='1', row='8')
     entry_free_sms.bind('<FocusIn>', on_entry_click('', entry_free_sms, values[7]))
     entry_free_sms.bind('<FocusOut>', on_focusout('', entry_free_sms, values[7]))
     label8 = tk.Label(new_window, text='Количество СМС')
     label8.grid(column='0', row='8', sticky='e')
-    entry_free_mms = tk.Entry(new_window, width='20', justify='right')
+
+    entry_free_mms = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_int)
     entry_free_mms.grid(column='1', row='9')
     entry_free_mms.bind('<FocusIn>', on_entry_click('', entry_free_mms, values[8]))
     entry_free_mms.bind('<FocusOut>', on_focusout('', entry_free_mms, values[8]))
     label9 = tk.Label(new_window, text='Количество ММС')
     label9.grid(column='0', row='9', sticky='e')
-    entry_price_sms = tk.Entry(new_window, width='20', justify='right')
+
+    entry_price_sms = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_float)
     entry_price_sms.grid(column='1', row='10')
     entry_price_sms.bind('<FocusIn>', on_entry_click('', entry_price_sms, values[9]))
     entry_price_sms.bind('<FocusOut>', on_focusout('', entry_price_sms, values[9]))
     label10 = tk.Label(new_window, text='Стоимость СМС, руб.')
     label10.grid(column='0', row='10', sticky='e')
-    entry_price_mms = tk.Entry(new_window, width='20', justify='right')
+
+    entry_price_mms = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_float)
     entry_price_mms.grid(column='1', row='11')
     entry_price_mms.bind('<FocusIn>', on_entry_click('', entry_price_mms, values[10]))
     entry_price_mms.bind('<FocusOut>', on_focusout('', entry_price_mms, values[10]))
     label11 = tk.Label(new_window, text='Стоимость ММС, руб.')
     label11.grid(column='0', row='11', sticky='e')
-    entry_free_mb = tk.Entry(new_window, width='20', justify='right')
+
+    entry_free_mb = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_int)
     entry_free_mb.grid(column='1', row='12')
     entry_free_mb.bind('<FocusIn>', on_entry_click('', entry_free_mb, values[11]))
     entry_free_mb.bind('<FocusOut>', on_focusout('', entry_free_mb, values[11]))
     label12 = tk.Label(new_window, text='Количество Мегабайт')
     label12.grid(column='0', row='12', sticky='e')
-    entry_price_mb = tk.Entry(new_window, width='20', justify='right')
+
+    entry_price_mb = tk.Entry(new_window, width='20', justify='right', validate="key", validatecommand=check_float)
     entry_price_mb.grid(column='1', row='13')
     entry_price_mb.bind('<FocusIn>', on_entry_click('', entry_price_mb, values[12]))
     entry_price_mb.bind('<FocusOut>', on_focusout('', entry_price_mb, values[12]))
@@ -231,9 +217,6 @@ def open_edit_window():
     button_exit = tk.Button(new_window, text='Выход', command=new_window.destroy, width='10')
     button_add.grid(column='0', row='14', pady='15', padx='10', sticky='e')
     button_exit.grid(column='1', row='14', pady='15', padx='10', sticky='w')
-
- 
-
 
 def open_fill_window():
     def show_modal_error():
@@ -387,8 +370,6 @@ file_menu.add_command(label='Удалить тариф', command=delete_line)
 file_menu.add_separator()
 file_menu.add_command(label="Выйти", command=quit)
 
-
-
 w_main.config(menu=main_menu)
 
 #TABLE AREA
@@ -420,11 +401,8 @@ t_main.column('price_mms', width='50', minwidth='50')
 t_main.column('free_mb', width='50', minwidth='50')
 t_main.column('price_mb', width='50', minwidth='50')
 
-
-
 t_main.grid(column='1', row='1', rowspan='3', sticky='esn', padx='4')
 
-#####
 scrollbar = ttk.Scrollbar(orient='vertical', command=t_main.yview)
 t_main.configure(yscroll=scrollbar.set)
 scrollbar.grid(row='1', column='6', sticky="nsw",rowspan='3')
@@ -433,7 +411,5 @@ def prevent_resize(event):
         return "break"
 t_main.bind('<Button-1>', prevent_resize)
 t_main.bind('<Motion>', prevent_resize)
-##№№№
-
 
 w_main.mainloop()
